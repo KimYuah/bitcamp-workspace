701e4a3657a3c770b19b269d89a496cf8e2d6693
KimYuah <www0157@naver.com>
2020-08-19 18:22:35 +0900

    200819

--- bitcamp-java-project/src/main/java/com/eomcs/pms/App_a.java
+++ bitcamp-java-project/src/main/java/com/eomcs/pms/App_a.java
@@ -6,7 +6,7 @@ import com.eomcs.pms.handler.ProjectHandler;
 import com.eomcs.pms.handler.TaskHandler;
 import com.eomcs.util.Prompt;
 
-public class App {
+public class App_a {
 
   public static void main(String[] args) {
 
@@ -19,8 +19,19 @@ public class App {
 
     MemberHandler memberHandler = new MemberHandler();
 
-    ProjectHandler projectHandler = new ProjectHandler(memberHandler);
-    TaskHandler taskHandler = new TaskHandler(memberHandler);
+    ProjectHandler projectHandler = new ProjectHandler();
+
+    // ProjectHandler의 의존 객체 주입을 막는다.
+    //projectHandler.memberHandler = memberHandler;
+
+    TaskHandler taskHandler = new TaskHandler();
+
+    // TaskHandler의 의존 객체 주입을 막는다.
+    //taskHandler.memberHandler = memberHandler;
+
+    // 인스턴스를 사용하는데 필요한 의존 객체 주입을 누락하더라도
+    // 컴파일러는 알 수가 없다.
+    // 실행할 때 비로서 의존 객체가 누락된 문제가 발생할 것이다.
 
     loop:
       while (true) {
@@ -52,8 +63,8 @@ public class App {
           default:
             System.out.println("실행할 수 없는 명령입니다.");
         }
-        System.out.println();
-    }
+        System.out.println(); // 이전 명령의 실행을 구분하기 위해 빈 줄 출력
+      }
 
     Prompt.close();
   }
