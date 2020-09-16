a66cadb44458e433e2ef1066cc682597980d2a35
KimYuah <www0157@naver.com>
2020-09-16 18:09:48 +0900

    200916

--- bitcamp-java-basic/src/test/java/Exam0610.java
+++ bitcamp-java-basic/src/test/java/Exam0610.java
@@ -1,28 +1,63 @@
-import java.io.File;
-import java.io.FilenameFilter;
 
 public class Exam0610 {
-  public static void main(String[] args) throws Exception {
 
-    class JavaFilter implements FilenameFilter {
+  static class Calculator {
+    double rate;
 
-      @Override
-      public boolean accept(File dir, String name) {
-
-        if (name.endsWith(".java"))
-          return true;
-        return false;        
-      }
+    public Calculator(double rate) {
+      this.rate = rate;
     }
-    File dir = new File(".");
 
-    JavaFilter javaFilter = new JavaFilter();
+    public double year(int money) {
+      return money * rate / 100; 
+    }
 
-    String[] names = dir.list(javaFilter);
+    public double month(int money) {
+      return money * rate / 12;
+    }
 
-    for (String name : names) {
-      System.out.println(name);
+    public double day(int money) {
+      return money * rate / 100 / 365;
     }
   }
+
+
+  static interface Interest {
+    double compute(int money);
+  }
+
+
+  public static void main(String[] args) {
+    Calculator 보통예금 = new Calculator(0.5);
+    Calculator 정기예금 = new Calculator(1.5);
+    Calculator 청년행복예금 = new Calculator(10);
+
+    System.out.println("[보통예금]");
+    Interest i1 = 보통예금::year;
+    System.out.printf("년 이자 : %.1f\n", i1.compute(10_0000_0000));
+
+    i1 = 보통예금::month;
+    System.out.printf("월 이자 : %.1f\n", i1.compute(10_0000_0000));
+
+    i1 = 보통예금::day;
+    System.out.printf("일 이자 : %.1f\n", i1.compute(10_0000_0000));
+
+    System.out.println("---------------------------");
+
+    System.out.println("[정기예금]");
+    Interest i2 = 정기예금::year;
+    System.out.printf("년 이자 : %.1f\n", i2.compute(10_0000_0000));
+
+    i2 = 정기예금::month;
+    System.out.printf("월 이자 : %.1f\n", i2.compute(10_0000_0000));
+
+    i2 = 정기예금::day;
+    System.out.printf("일 이자 : %.1f\n", i2.compute(10_0000_0000));
+
+    System.out.println("---------------------------");
+
+
+  }
+
 }
 
