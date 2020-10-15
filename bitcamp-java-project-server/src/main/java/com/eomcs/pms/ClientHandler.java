fecd3b4ceb25f0ab5a3b5cf9b14e05ea56c14070
KimYuah <www0157@naver.com>
2020-10-15 17:06:37 +0900

    201015

--- bitcamp-java-project-server/src/main/java/com/eomcs/pms/ClientHandler.java
+++ bitcamp-java-project-server/src/main/java/com/eomcs/pms/ClientHandler.java
@@ -6,9 +6,8 @@ import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.Socket;
 
-// 클라이언트의 요청을 처리하는 일을 한다.
+// 클라이언트 요청을 처리하는 일을 한다.
 public class ClientHandler implements Runnable {
-
   Socket socket;
 
   public ClientHandler(Socket socket) {
@@ -17,14 +16,14 @@ public class ClientHandler implements Runnable {
 
   @Override
   public void run() {
+
     InetAddress address = this.socket.getInetAddress();
     System.out.printf("클라이언트(%s)가 연결되었습니다.\n",
         address.getHostAddress());
 
     try (Socket socket = this.socket; // try 블록을 떠날 때 close()가 자동 호출된다.
         BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-        PrintWriter out = new PrintWriter(socket.getOutputStream());) {
-
+        PrintWriter out = new PrintWriter(socket.getOutputStream())) {
 
       while (true) {
         String request = in.readLine();
@@ -36,9 +35,9 @@ public class ClientHandler implements Runnable {
       }
 
 
-   } catch (Exception e) {
-       System.out.println("클라이언트와의 통신 오류!");
-   }
+    } catch (Exception e) {
+      System.out.println("클라이언트와의 통신 오류!");
+    }
 
     System.out.printf("클라이언트(%s)와의 연결을 끊었습니다.\n",
         address.getHostAddress());
--- bitcamp-java-project-server/src/main/java/com/eomcs/pms/listener/DataHandlerListener.java
+++ bitcamp-java-project-server/src/main/java/com/eomcs/pms/listener/DataHandlerListener.java
@@ -34,9 +34,8 @@ public class DataHandlerListener implements ApplicationContextListener {
   List<Task> taskList = new ArrayList<>();
   File taskFile = new File("./task.json"); // 작업을 저장할 파일 정보
 
-
   @Override
-  public void contextInitialized(Map<String, Object> context) {
+  public void contextInitialized(Map<String,Object> context) {
     // 애플리케이션의 서비스가 시작되면 먼저 파일에서 데이터를 로딩한다.
     // 파일에서 데이터 로딩
     loadData(boardList, boardFile, Board[].class);
@@ -44,19 +43,18 @@ public class DataHandlerListener implements ApplicationContextListener {
     loadData(projectList, projectFile, Project[].class);
     loadData(taskList, taskFile, Task[].class);
 
-    // 옵저버가 파일에서 데이터(게시글, 회원, 프로젝트, 작업)를 읽어
+    // 옵저버가 파일에서 데이터(게시글,회원,프로젝트,작업)를 읽어
     // List 컬렉션에 저장한 다음,
-    // 발행자(App 객체)가 사용할 수 있도록 앱 객체에 담아서 공유한다.
+    // 발행자(App 객체)가 사용할 수 있도록 맵 객체에 담아서 공유한다.
     context.put("boardList", boardList);
     context.put("memberList", memberList);
     context.put("projectList", projectList);
     context.put("taskList", taskList);
-
   }
 
   @Override
-  public void contextDestroyed(Map<String, Object> context) {
-    // 애플리케이션 서비스가 종료되면 컬렉션 보관된 객체를 파일에 저장한다.
+  public void contextDestroyed(Map<String,Object> context) {
+    // 애플리케이션 서비스가 종료되면 컬렉션에 보관된 객체를 파일에 저장한다.
     // 데이터를 파일에 저장
     saveData(boardList, boardFile);
     saveData(memberList, memberFile);
@@ -64,62 +62,55 @@ public class DataHandlerListener implements ApplicationContextListener {
     saveData(taskList, taskFile);
   }
 
+  private <T> void loadData(
+      Collection<T> list, // 객체를 담을 컬렉션
+      File file, // JSON 문자열이 저장된 파일
+      Class<T[]> clazz // JSON 문자열을 어떤 타입의 배열로 만들 것인지 알려주는 클래스 정보
+      ) {
+    BufferedReader in = null;
 
- private <T> void loadData (
-     Collection<T> list, // 객체를 담을 컬렉션
-     File file, // JSON 문자열이 저장된 파일
-     Class<T[]> clazz // JSON 문자열을 어떤 타입의 배열로 만들 것인지 알려주는 클래스 정보
-     ) {
-   BufferedReader in = null;
+    try {
+      in = new BufferedReader(new FileReader(file));
+      list.addAll(Arrays.asList(new Gson().fromJson(in, clazz)));
+      System.out.printf("'%s' 파일에서 총 %d 개의 객체를 로딩했습니다.\n",
+          file.getName(), list.size());
 
-   try {
-     in = new BufferedReader(new FileReader(file));
+    } catch (Exception e) {
+      System.out.printf("'%s' 파일 읽기 중 오류 발생! - %s\n",
+          file.getName(), e.getMessage());
 
-     list.addAll(Arrays.asList(new Gson().fromJson(in, clazz)));
+    } finally {
+      try {
+        in.close();
+      } catch (Exception e) {
+      }
+    }
+  }
 
-     System.out.printf("'%s' 파일에서 총 %d 개의 객체를 로딩했습니다.\n",
-         file.getName(), list.size());
+  private void saveData(Collection<?> list, File file) {
+    BufferedWriter out = null;
 
-   } catch (Exception e) {
-     System.out.printf("'%s' 파일 읽기 중 오류 발생! - %s\n",
-         file.getName(), e.getMessage());
+    try {
+      out = new BufferedWriter(new FileWriter(file));
 
-   } finally {
-     try {
-       in.close();
-     } catch (Exception e) {
-     }
-   }
- }
+      Gson gson = new Gson();
+      String jsonStr = gson.toJson(list);
+      out.write(jsonStr);
 
+      out.flush();
 
- private void saveData(Collection<?> list, File file) {
-   BufferedWriter out = null;
+      System.out.printf("총 %d 개의 객체를 '%s' 파일에 저장했습니다.\n",
+          list.size(), file.getName());
 
-   try {
-     out = new BufferedWriter(new FileWriter(file));
+    } catch (IOException e) {
+      System.out.printf("객체를 '%s' 파일에  쓰는 중 오류 발생! - %s\n",
+          file.getName(), e.getMessage());
 
-     // 컬렉션 객체를 통째로 JSON 문자열로 내보내기
-     Gson gson = new Gson();
-     String jsonStr = gson.toJson(list);
-     out.write(jsonStr);
-
-     out.flush();
-
-     System.out.printf("총 %d 개의 객체를 '%s' 파일에 저장했습니다.\n",
-         list.size(), file.getName());
-
-   } catch (IOException e) {
-     System.out.printf("객체를 '%s' 파일에  쓰는 중 오류 발생! - %s\n",
-         file.getName(), e.getMessage());
-
-   } finally {
-     try {
-       out.close();
-     } catch (IOException e) {
-     }
-   }
- }
-
-
+    } finally {
+      try {
+        out.close();
+      } catch (IOException e) {
+      }
+    }
+  }
 }
