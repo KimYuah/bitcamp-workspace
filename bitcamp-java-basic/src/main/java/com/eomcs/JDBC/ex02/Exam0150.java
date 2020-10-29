90b2c831c52453185b0a10a88b51e4b641504e9f
KimYuah <www0157@naver.com>
2020-10-29 18:08:48 +0900

    201029

--- bitcamp-java-basic/src/main/java/com/eomcs/JDBC/ex02/Exam0150.java
+++ bitcamp-java-basic/src/main/java/com/eomcs/JDBC/ex02/Exam0150.java
@@ -30,7 +30,7 @@ public class Exam0150 {
       if (count == 0) {
         System.out.println("해당 번호의 게시물이 존재하지 않습니다.");
       } else {
-      System.out.println("삭제하였습니다.");
+        System.out.println("삭제하였습니다.");
       }
     }
   }
