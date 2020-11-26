8e8554d85157a8a2e906288d89926ee0458aa17b
KimYuah <www0157@naver.com>
2020-11-26 20:19:20 +0900

    201126

---
+++ bitcamp-java-project-server/src/main/java/com/eomcs/pms/web/HelloCommand.java
@@ -0,0 +1,46 @@
+package com.eomcs.pms.web;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.Servlet;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebServlet;
+
+@WebServlet("/hello")
+// Command 규칙에 따라 클래스를 정의한다.
+public class HelloCommand implements Servlet {
+
+  @Override
+  public void service(ServletRequest request, ServletResponse response)
+      throws ServletException, IOException {
+
+    response.setContentType("text/plain;charset=UTF-8");
+    PrintWriter out = response.getWriter();
+    out.println("안녕하세요!");
+  }
+
+  @Override
+  public void init(ServletConfig config) throws ServletException {
+  }
+
+  @Override
+  public ServletConfig getServletConfig() {
+    return null;
+  }
+
+  @Override
+  public String getServletInfo() {
+    return null;
+  }
+
+  @Override
+  public void destroy() {
+
+  }
+
+
+
+}
